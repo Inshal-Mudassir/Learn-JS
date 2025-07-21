651dffac01bd55fbd503d48070a023d582ff4894
Inshal Mudassir <135925634+Inshal-Mudassir@users.noreply.github.com>
2025-07-21 21:48:03 +0500

    Add files via upload

---
+++ 07_functions.js
@@ -0,0 +1,36 @@
+// 1. Function Basics
+
+// What is a Function?
+
+// A function is a block of code that performs a specific task.
+// You write it once, and you can reuse it as many times as you want.
+
+// Why Use Functions?
+
+// Avoid repeating code (DRY: Don’t Repeat Yourself)
+// Improve readability and maintainability
+// Divide big problems into smaller reusable parts
+// Make testing and debugging easier
+
+// Function Syntax
+
+// Declare A Function
+function greet() {
+  console.log("Hello, Inshal!");
+}
+
+// Calling a Function
+greet(); // Output: Hello, Inshal!
+
+// 2. FunctionParametersAndArguments
+
+// What Are Parameters & Arguments?
+
+// Parameters are placeholders you define in a function — like variables.
+// Arguments are actual values you pass when calling the function.
+
+// function greetUser(name) {
+//   console.log(`Hello, ${name}!`);
+// }
+
+// greetUser("Inshal"); "Inshal" is the argument
