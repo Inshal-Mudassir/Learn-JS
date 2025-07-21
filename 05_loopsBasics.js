2af9564342eafabeb74ee5ced213ae2384575513
Inshal Mudassir <135925634+Inshal-Mudassir@users.noreply.github.com>
2025-07-21 15:59:47 +0500

    Add files via upload

---
+++ 05_loopsBasics.js
@@ -0,0 +1,28 @@
+// 1. for Loop – Basic Syntax
+
+for (initialization; condition; increment) {
+  // code to run each time
+}
+
+for (let i = 1; i <= 5; i++) {
+  console.log("Step", i);
+}
+
+// 2. while Loop – Basic Syntax
+
+let i = 1;
+
+while (i <= 5) {
+  console.log(i);
+  i++;
+}
+
+// 3. do...while Loop – Basic Syntax
+
+let i = 1;
+
+do {
+  console.log(i);
+  i++;
+} while (i <= 5);
+
