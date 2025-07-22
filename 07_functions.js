85f23c6c2d10f8a362cd619ae307357a9b776ae4
Inshal Mudassir <135925634+Inshal-Mudassir@users.noreply.github.com>
2025-07-22 21:18:09 +0500

    Update 07_functions.js

--- 07_functions.js
+++ 07_functions.js
@@ -29,8 +29,64 @@ greet(); // Output: Hello, Inshal!
 // Parameters are placeholders you define in a function — like variables.
 // Arguments are actual values you pass when calling the function.
 
-// function greetUser(name) {
-//   console.log(`Hello, ${name}!`);
-// }
+function greetUser(name) {
+  console.log(`Hello, ${name}! Welcome back`);
+}
 
-// greetUser("Inshal"); "Inshal" is the argument
+greetUser("Inshal"); // "Inshal" is the argument
+
+// Function Expression
+
+// A function expression is when you assign a function to a variable.
+
+const greet = function() {
+  console.log("Hello from a function expression!");
+};
+
+greet(); // Output: Hello from a function expression!
+
+// Named vs Anonymous Functions
+
+// Named Function
+
+// A named function has an identifier after the function keyword.
+
+function sayHello() {
+  console.log("Hello!");
+}
+sayHello(); // Output: Hello!
+
+// Anonymous Function
+
+// An anonymous function has no name. It’s often used in function expressions or callbacks.
+
+const greet = function() {
+  console.log("Hi!");
+};
+greet(); // Output: Hi!
+
+// The return Statement in JavaScript
+
+// The return statement sends a value back from a function to wherever it was called. It ends the function's execution.
+
+function add(a, b) {
+  return a + b;
+}
+
+let result = add(5, 3);
+console.log(result);  // 8
+
+// Default Parameters in JavaScript
+
+// Default parameters allow you to set a fallback value for a parameter when no argument is provided.
+
+function greet(name = "Guest") {
+  console.log(`Hello, ${name}!`);
+}
+
+greet("Inshal");  // Hello, Inshal!
+greet();          // Hello, Guest!
+
+
+
+
